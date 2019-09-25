3065a3a7f3416c7b4f736eae7737e81177b6233c
Olympia <ocpnpf@mail.missouri.edu>
2019-09-25 17:50:43 -0500

    base commit

---
+++ data structures assignment 3/data structures assignment 3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  data structures assignment 3 code
+//
+//  Created by Olympia Pyburn on 9/25/19.
+//  Copyright © 2019 Olympia Pyburn. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
